0ebeba50597e43fbe3e3f6a05fdda4b21ae6f645
lrh <153011490@qq.com>
2020-03-27 00:48:56 +0800

    add navigation component and router

---
+++ ordering_frontend/src/views/SellerPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <Navigation></Navigation>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navigation from '@/components/common/Navigation.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Navigation
+  }
+}
+</script>
